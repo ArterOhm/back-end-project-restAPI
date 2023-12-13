cc7acaa6efed9e70c1f095cc850cee68a26b4d9b
ArterOhm <Aef117722@gmail.com>
2023-12-14 00:04:12 +0700

    :zap: :rocket: On server

---
+++ modules/servers/module.go
@@ -0,0 +1,28 @@
+package servers
+
+import (
+	monitorHandlers "github.com/ArterOhm/back-end-project-restAPI/modules/monitor/monitor_handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+type IModuleFactory interface {
+	MonitorModule()
+}
+
+type moduleFactory struct {
+	router fiber.Router
+	server *server
+}
+
+func InitModule(r fiber.Router, s *server) IModuleFactory {
+	return &moduleFactory{
+		router: r,
+		server: s,
+	}
+}
+
+func (m *moduleFactory) MonitorModule() {
+	handler := monitorHandlers.MonitorHandler(m.server.cfg)
+
+	m.router.Get("/", handler.HealthCheck)
+}
